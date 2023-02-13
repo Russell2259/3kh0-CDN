e2f46f56d41a38095aeb898696dacab4b69c5d8f
Russell2259 <russell2259@tuta.io>
2023-02-13 14:03:28 -0700

    ._.

--- docs.js
+++ docs.js
@@ -1,5 +1,8 @@
-if (window.location.pathname.endsWith('/') && !window.location.pathname.endsWith('/wiki/')) {
+if (window.location.pathname.endsWith('/')) {
     alert('asdfghjkl')
     const readme = document.querySelector('#readme');
     alert(readme)
 }
+
+alert('asdfghjkl;')
+alert(window.location.pathname.endsWith('/'))
