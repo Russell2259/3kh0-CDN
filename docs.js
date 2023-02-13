789ff43b4ea06f71bd7509194651f143cb725dfc
Russell2259 <russell2259@tuta.io>
2023-02-13 14:12:58 -0700

    add readme

--- docs.js
+++ docs.js
@@ -1,8 +1,8 @@
 if (window.location.pathname.endsWith('/')) {
-    alert('asdfghjkl')
     const readme = document.querySelector('#readme');
-    alert(readme)
+    fetch('https://raw.githubusercontent.com/Russell2259/3kh0-CDN/main/README.md')
+    .then(res => res.text())
+    .then(data => {
+        readme.innerHTML = data;
+    })
 }
-
-alert('asdfghjkl;')
-alert(window.location.pathname.endsWith('/'))
