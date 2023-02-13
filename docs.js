07b4afe05ddc3353293b040430a70804a8c9dcad
Russell2259 <russell2259@tuta.io>
2023-02-13 14:56:09 -0700

    add docs link

--- docs.js
+++ docs.js
@@ -1,15 +1,3 @@
-if (window.location.pathname.endsWith('/')) {
-    const readme = document.querySelector('#readme');
-    fetch('https://raw.githubusercontent.com/Russell2259/3kh0-CDN/main/README.md')
-        .then(res => res.text())
-        .then(data => {
-            readme.innerHTML = data;
-        })
-        .catch(e => {
-            throw new Error(e);
-        })
-}
-
 window.onerror = (e) => {
-    alert(e)
+    alert(e);
 }
