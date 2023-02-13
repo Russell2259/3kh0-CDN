6bdf845139a67c6c66b688cc88f7ddbbe38499b9
Russell2259 <russell2259@tuta.io>
2023-02-13 14:21:10 -0700

    test

--- docs.js
+++ docs.js
@@ -1,8 +1,9 @@
 if (window.location.pathname.endsWith('/')) {
     const readme = document.querySelector('#readme');
     fetch('https://raw.githubusercontent.com/Russell2259/3kh0-CDN/main/README.md')
-    .then(res => res.text())
-    .then(data => {
-        readme.innerHTML = data;
-    })
+        .then(res => res.text())
+        .then(data => {
+            readme.innerHTML = data;
+        })
+    alert('bru');
 }
