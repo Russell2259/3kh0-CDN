23de01c859a82549872657e4b86bd1ca725aef8a
Russell2259 <russell2259@tuta.io>
2023-03-02 16:24:28 +0000

    add express middleware support

--- index.js
+++ index.js
@@ -1,32 +1,49 @@
-import express from 'express';
 import fetch from 'node-fetch';
 
-const app = express();
 const repository = '3kh0/3kh0-Assets/main';
 
-app.all('/', async (req, res) => {
-    const file = await fetch('https://raw.githubusercontent.com/3kh0/3kh0.github.io/main/assets/games.json');
-    const games = await file.json();
-    res.json(games);
-})
-
-app.all('*', async (req, res) => {
+const express_ = (path = '') => {
     try {
-        const file = await fetch(`https://raw.githack.com/${repository}${req.originalUrl}`);
-        const data = new Buffer.from(await file.arrayBuffer());
-        
-        if (file.status !== 404) {
-            res.writeHead(file.status, { 'Content-Type': file.headers.get('content-type').split(';')[0] })
-            res.end(data);
-        } else {
-            return res.sendStatus(404);
+        return async function (req, res, next) {
+            if (req.path == path) {
+                try {
+                    const file = await fetch('https://raw.githubusercontent.com/3kh0/3kh0.github.io/main/assets/json/games.json');
+                    const games = await file.json();
+                    res.json(games);
+                } catch (e) {
+                    res.sendStatus(404);
+                }
+            }
+
+            if (req.path.startsWith(path) && req.path != path) {
+                try {
+                    const file = await fetch(`https://raw.githack.com/${repository}/${req.originalUrl.replace(path, '')}`);
+                    const data = new Buffer.from(await file.arrayBuffer());
+
+                    if (file.status !== 404) {
+                        res.writeHead(file.status, { 'Content-Type': file.headers.get('content-type').split(';')[0] })
+                        res.end(data);
+                    } else {
+                        res.sendStatus(404);
+                    }
+                } catch (e) {
+                    res.sendStatus(404);
+
+                    console.error(e);
+                }
+            }
         }
     } catch (e) {
-        res.sendStatus(404);
-        throw new Error(e);
+        throw e;
     }
-})
+}
 
-app.listen(9000, () => {
-    console.log('Your 3kh0.github.io CDN server is running on port 9000')
-})
+class CDN {
+    constructor() {
+        this.express = express_;
+
+        this.repository = repository;
+    }
+}
+
+export default new CDN();
